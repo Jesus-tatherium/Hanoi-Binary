e0f6a943a909a07a8f6c5a5224be7a92459fcdf2
Clement Amphoux <klm.amphoux@gmail.com>
2018-10-27 17:56:01 +0200

    add all projet files

---
+++ ProjetTypeCpp/Utils/Utils.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "Utils.hpp"
+
+void InvalidNumber()
+{
+	system("cls");
+	cout << "Invalid number, read the possiblities again please" << endl;
+	cout << "Press any key to continue" << endl;
+	system("pause");
+}
+
---
+++ ProjetTypeCpp/Utils/Utils.hpp
@@ -0,0 +1,23 @@
+/*
+* @author Clement Amphoux
+
+* @file Utils.hpp
+
+* @date 09/10/2018
+
+* @brief
+
+*/
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <iostream>
+
+#define MAXIMUM_OF_INT 2147483647
+
+using namespace std;
+
+void InvalidNumber();
+
+#endif // !UTILS_H
